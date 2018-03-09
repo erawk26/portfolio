1d3d6d3f67632a569116e08fc4db1731225bf4e5
erik <erik@hark.bz>
2018-03-08 20:49:16 -0500

    forgot to commit controller changes

--- js/work.controller.js
+++ js/work.controller.js
@@ -16,7 +16,7 @@ var app = new Vue({
 				// tags: ["foo", "bar"],
 				// imgs: [],
 				desc:
-					"I replatformed the entire site from their old custom CMS into Drupal 8 in May 2017. They needed an easier way to manage their SEO and content and Drupal fit the bill. In November of 2017 I built off the same backend but rethemed the site. This was a massive undertaking. The PSD mockup was 37 pages for mobile/desktop."
+					"I replatformed the entire site from their old custom CMS into Drupal 8 in May 2017. They needed an easier way to manage their SEO and content and Drupal fit the bill. In November of 2017, I built off the same backend but rethemed the site. This was a massive undertaking. The PSD mockup was 37 pages for mobile/desktop."
 			},
 			{
 				// -----------JOB-labrea-----------
@@ -26,7 +26,7 @@ var app = new Vue({
 				logo: "labrea-logo.png",
 				title: "La Brea Bakery",
 				desc:
-					"Added images galleries to the Bread sections. Re-Templated the Recipes pages. Made 5 different landing pages for various product promotions."
+					"I Added images galleries to the Bread sections. Re-Templated the Recipes pages. Made 5 different landing pages for various product promotions."
 			},
 			{
 				// -----------JOB-zevia-----------
@@ -36,7 +36,7 @@ var app = new Vue({
 				logo: "zevia-logo.png",
 				title: "Zevia",
 				desc:
-					"Made multiple landing pages to promote things like Ferdinand the movie, Health, etc. The Sugar Reduction Landing page featured the youtube API and needed to maintain a borderless aspect ratio, as well as working across all mobile and desktop devices. I redid their current blog pages (teaser & full template). I also created bi-monthly Mailchimp templates for them in HTML from their PSDs."
+					"I Made multiple landing pages to promote things like Ferdinand the movie, Health, etc. The Sugar Reduction Landing page featured the youtube API and needed to maintain a borderless aspect ratio, as well as working across all mobile and desktop devices. I redid their current blog pages (teaser & full template). I also created bi-monthly Mailchimp templates for them in HTML from their PSDs."
 			},
 			{
 				// -----------JOB-hark-----------
@@ -46,7 +46,7 @@ var app = new Vue({
 				logo: "hark-logo.png",
 				title: "Hark Digital",
 				desc:
-					"Rebuilt the current version of their site on Wordpress using wordpressify a npm theme compiler for WP. I leveraged Masonry/Infinite scroll together to manage larger lists of content. I built this site with limited modules and lots of custom queries/templating. The contact page has a nice label slide up effect when entering input."
+					"I rebuilt the current version of their site on Wordpress using wordpressify a node compiler for WP. I leveraged Masonry/Infinite scroll together to manage larger lists of content. I built this site with limited modules and lots of custom queries/templating. The contact page has a nice label slide up effect when entering input."
 			},
 			{
 				// -----------JOB-nsb-----------
@@ -66,7 +66,7 @@ var app = new Vue({
 				logo: "cbv-logo.png",
 				title: "Community Barn Ventures",
 				desc:
-					"I built the Client Landing Page, Individual Pages, sliders, client blocks, and Testimonials. The LP has a client grid that leverages CSS Grid and jQuery. I wrote some JS to arrange the bigger clients into predefined spaces then fills the smaller clients in around them, without any unwanted spaces. Its also completely responsive and will rearrange the grid based on screen size.<br><a href='//codepen.io/erawk26/pen/LQoXEw'>CodePen DEMO</a>"
+					"I built the Client Landing Page, Individual Pages, sliders, client blocks, and Testimonials. The LP has a client grid that leverages CSS Grid and jQuery. I wrote some JS to arrange the bigger clients into predefined spaces then fills the smaller clients in around them, without any unwanted spaces. Its also completely responsive and will rearrange the grid based on screen size.<br><a target='_blank' href='//codepen.io/erawk26/pen/LQoXEw'>CodePen Example</a>"
 			},
 			{
 				// -----------JOB-mbf-----------
@@ -79,16 +79,6 @@ var app = new Vue({
 					"Added a mobile menu to their site. This proved to be quite intensive due to the way their current menu plugin was working. I also reconfigured their content templates to give the author custom control of each sections background color, borders, etc."
 			},
 			{
-				// -----------JOB-baristas-----------
-				machine_name: "baristas",
-				href: "//baristasbeans.com",
-				img: "port1.jpg",
-				logo: "baristas-logo.png",
-				title: "Barista's Beans",
-				desc:
-					"My eCommerce mobile-first site built off of Drupal. It features recurring coffee subscriptions. It's very secure and easy to maintain. Everything gets backed up and updated through SSH."
-			},
-			{
 				// -----------JOB-4sight-----------
 				machine_name: "foursight",
 				href: "//www.go4sight.com/blog",
@@ -96,7 +86,18 @@ var app = new Vue({
 				logo: "4sight-logo.png",
 				title: "4 Sight",
 				desc: "They had a Drupal 7 site with a lot of resources (articles, blog posts, case studies, etc) that needed to be filtered better. I added and improved upon their existing filters as well as reformatting their blog templates."
-			}
+			},
+			{
+				// -----------JOB-baristas-----------
+				machine_name: "baristas",
+				href: "//baristasbeans.com",
+				img: "port1.jpg",
+				logo: "baristas-logo.png",
+				title: "Barista's Beans",
+				desc:
+					"My eCommerce mobile-first site built off of Drupal 7. It featured recurring coffee subscriptions. It was very secure and easy to maintain. Everything got backed up and updated through SSH/Drush."
+			},
+
 		],
 	},
 	computed: {
