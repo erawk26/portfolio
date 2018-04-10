6771566aed6e6e47e6369eba649de121d1c178fe
erik <erik@hark.bz>
2018-04-10 16:12:11 -0400

    wording

--- js/work.controller.js
+++ js/work.controller.js
@@ -14,7 +14,7 @@ var vm_data= {
 			//tags: ["Drupal 8"],
 			// imgs: [],
 			desc:
-				"I replatformed the entire site from their old custom CMS into Drupal 8 in May 2017. They needed an easier way to manage their SEO and content and Drupal fit the bill. In November of 2017, I built off the same backend but rethemed the site. This was a massive undertaking. The PSD mockup was 37 pages for mobile/desktop."
+				"While working at Hark, I replatformed the entire site from their old custom CMS into Drupal 8 in May 2017. They needed an easier way to manage their SEO and content and Drupal fit the bill. In November of 2017, I built off the same backend but rethemed the site. This was a massive undertaking. The PSD mockup was 37 pages for mobile/desktop."
 		},
 		{
 			// -----------JOB-labrea-----------
@@ -24,7 +24,7 @@ var vm_data= {
 			logo: "labrea-logo.png",
 			title: "La Brea Bakery",
 			desc:
-				"I Added images galleries to the Bread sections. Re-Templated the Recipes pages. Made 5 different landing pages for various product promotions."
+				"I coded images galleries to the Bread sections. Re-Templated the Recipes pages. I also developed 5 different landing pages for various product promotions."
 		},
 		{
 			// -----------JOB-zevia-----------
@@ -34,7 +34,7 @@ var vm_data= {
 			logo: "zevia-logo.png",
 			title: "Zevia",
 			desc:
-				"I Made multiple landing pages to promote things like Ferdinand the movie, Health, etc. The Sugar Reduction Landing page featured the youtube API and needed to maintain a borderless aspect ratio, as well as working across all mobile and desktop devices. I redid their current blog pages (teaser & full template). I also created bi-monthly Mailchimp templates for them in HTML from their PSDs."
+				"I developed multiple landing pages to promote things like Ferdinand the movie, Health, etc. The Sugar Reduction landing page featured the youtube API and needed to maintain a borderless aspect ratio, as well as working across all mobile and desktop devices. I redid their current blog pages (teaser & full template). I also created bi-monthly Mailchimp templates for them in HTML from their PSDs."
 		},
 		{
 			// -----------JOB-hark-----------
@@ -44,7 +44,7 @@ var vm_data= {
 			logo: "hark-logo.png",
 			title: "Hark Digital",
 			desc:
-				"I rebuilt the current version of their site on Wordpress using wordpressify a node compiler for WP. I leveraged Masonry/Infinite scroll together to manage larger lists of content. I built this site with limited modules and lots of custom queries/templating. The contact page has a nice label slide up effect when entering input."
+				"I helped replatform the current version of their site on Wordpress using wordpressify a node compiler for WP. I leveraged Masonry/Infinite scroll together to manage larger lists of content. I coded this site with limited modules and lots of custom queries/templating. The contact page has a nice label slide up effect when entering input."
 		},
 		{
 			// -----------JOB-nsb-----------
@@ -54,7 +54,7 @@ var vm_data= {
 			logo: "nsb-logo.png",
 			title: "Northfield Savings Bank",
 			desc:
-				"Pathway Mortgage landing page. A fully responsive page to create a call to action for first time home buyers. Features a nicely styled jquery accordion list."
+				"I coded the Pathway Mortgage landing page. A fully responsive page to create a call to action for first time home buyers. Features a nicely styled jquery accordion list."
 		},
 		{
 			// -----------JOB-cbv-----------
@@ -64,7 +64,7 @@ var vm_data= {
 			logo: "cbv-logo.png",
 			title: "Community Barn Ventures",
 			desc:
-				"I built the Client Landing Page, Individual Pages, sliders, client blocks, and Testimonials. The LP has a client grid that leverages CSS Grid and jQuery. I wrote some JS to arrange the bigger clients into predefined spaces then fills the smaller clients in around them, without any unwanted spaces. Its also completely responsive and will rearrange the grid based on screen size.<br><a target='_blank' href='//codepen.io/erawk26/pen/LQoXEw'>CodePen Example</a>"
+				"I coded the Client Landing Page, Individual Pages, sliders, client blocks, and Testimonials. The LP has a client grid that leverages CSS Grid and jQuery. I wrote some JS to arrange the bigger clients into predefined spaces then fills the smaller clients in around them, without any unwanted spaces. Its also completely responsive and will rearrange the grid based on screen size.<br><a target='_blank' href='//codepen.io/erawk26/pen/LQoXEw'>CodePen Example</a>"
 		},
 		{
 			// -----------JOB-mbf-----------
@@ -74,7 +74,7 @@ var vm_data= {
 			logo: "mbf-logo.png",
 			title: "MBF Bioscience",
 			desc:
-				"Added a mobile menu to their site. This proved to be quite intensive due to the way their current menu plugin was working. I also reconfigured their content templates to give the author custom control of each sections background color, borders, etc."
+				"We Added a mobile menu to their site. This proved to be quite intensive due to the way their current menu plugin was working. We also reconfigured their content templates to give the author custom control of each sections background color, borders, etc."
 		},
 		{
 			// -----------JOB-4sight-----------
@@ -83,7 +83,7 @@ var vm_data= {
 			img: "4sight.jpg",
 			logo: "4sight-logo.png",
 			title: "4 Sight",
-			desc: "They had a Drupal 7 site with a lot of resources (articles, blog posts, case studies, etc) that needed to be filtered better. I added and improved upon their existing filters as well as reformatting their blog templates."
+			desc: "They had a Drupal 7 site with a lot of resources (articles, blog posts, case studies, etc) that needed to be filtered better. We added and improved upon their existing filters as well as reformatting their blog templates."
 		},
 		{
 			// -----------JOB-baristas-----------
