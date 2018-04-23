50e2b7b48a1246f6ebd7de77be5b720d0102657a
erik <erik@hark.bz>
2018-04-23 14:23:52 -0400

    Still working at compiling single file Vue components in webpack. Getting closer.

---
+++ src/js/card.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <a :href="href" :title="'Visit ' + title" target="_blank" class="portfolio-card">
+            <div v-if="img" :style="{ 'background-image': 'url(/assets/img/' + img + ')' }" class="img">
+                <div class="ar"></div>
+            </div>
+            <h3>{{ title }}</h3>
+            <span v-if="skills" class="item">
+      <label for="scope" class="scope">Scope of Work:</label>
+      <p id="scope" v-html="scope" class="scope"></p>
+    </span>
+            <span v-if="skills" class="item">
+      <label for="skills">Skill Improvement:</label>
+      <p id="skills" v-html="skills"></p></span>
+        </a>
+    </div>
+</template>
+<script>
+    import Projects from './projects.js';
+	module.exports = {
+		name:'portfolio-card',
+		data: function() {
+			const card = Projects.projectArr.filter((job) => job.machine_name === active)[0];
+			console.log(card);
+			return card;
+		}
+	}
+</script>
+<style lang="scss">
+.portfolio-card {
+    width: 100%;
+    .img {
+        background-position: top center;
+        background-size: cover;
+        overflow: hidden;
+        transition: all 0s linear;
+        .ar {
+            display: block;
+            margin:0;
+            padding:0;
+            padding-bottom:42%;
+            width: 100%;
+            height: 0;
+        }
+    }
+    @media (min-width:480px) {
+        p {
+            font-size: 18px;
+        }
+    }
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,64 @@
-const path = require('path'),
-	outputDir= path.join(__dirname, 'public');
+const path = require("path"),
+	HtmlWebpackPlugin = require("html-webpack-plugin"),
+	outputDir = path.join(__dirname, "assets"),
+	inputDir = path.join(__dirname, "src");
 module.exports = {
-	entry: ['./src/index.js','./src/scss/style.scss'],
+	entry: [
+		inputDir + "/js/index.js",
+		inputDir + "/scss/style.scss"
+	],
 	output: {
-		path: path.join(__dirname, 'assets'),
-		filename: 'bundle.js'
+		path: outputDir + "/js",
+		filename: "bundle.js"
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.pug",
+			filename: "../../index.html",
+			inject: true
+		})
+	].filter(p => !!p),
 	module: {
 		rules: [
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[sha512:hash:base64:7].[ext]',
+							outputPath: '../img',
+							publicPath: 'assets/img',
+							useRelativePath: process.env.NODE_ENV === "production"
+						}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: '65-90',
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75
+							}
+						}
+					},
+				],
+			},
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -17,29 +68,97 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
+				exclude: /node_modules/,
 				use: [
 					{
-						loader: 'file-loader',
+						loader: "file-loader",
 						options: {
-							name: '[name].css',
-							outputPath: './'
+							name: "[name].css",
+							outputPath: "../css/"
 						}
 					},
 					{
-						loader: 'extract-loader'
+						loader: "extract-loader"
 					},
 					{
-						loader: 'css-loader'
+						loader: "css-loader"
 					},
 					{
-						loader: 'postcss-loader'
+						loader: "postcss-loader"
 					},
 					{
-						loader: 'sass-loader'
+						loader: "sass-loader"
 					}
 				]
+			},
+			{
+				test: /\.vue$/,
+				exclude: /node_modules/,
+				loader: 'vue-loader',
+				options: {
+					// `loaders` will overwrite the default loaders.
+					// The following config will cause all `<script>` tags without `lang`
+					// attribute to be loaded with `coffee-loader`
+					loaders: {
+						scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+						sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax', // <style lang="sass">
+						js: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['env']
+							}
+						},
+						pug: {
+							loader: "pug-loader",
+							options: {
+								doctype: 'pug',
+								pretty: true
+							}
+						},
+					},
+
+					// `preLoaders` are attached before the default loaders.
+					// You can use this to pre-process language blocks - a common use
+					// case would be build-time i18n.
+//					preLoaders: {
+//						js: '/path/to/custom/loader'
+//					},
+
+					// `postLoaders` are attached after the default loaders.
+					//
+					// - For `html`, the result returned by the default loader
+					//   will be compiled JavaScript render function code.
+					//
+					// - For `css`, the result will be returned by `vue-style-loader`
+					//   which isn't particularly useful in most cases. Using a PostCSS
+					//   plugin will be a better option.
+					postLoaders: {
+						html: 'babel-loader'
+					},
+
+					// `excludedPreLoaders` should be regex
+					excludedPreLoaders: /(eslint-loader)/
+				}
+			},
+			{
+				test: /\.pug$/,
+				include: inputDir,
+				use:
+					{
+						loader: "pug-loader",
+						options: {
+							doctype: 'pug',
+							pretty: true
+						}
+					}
 			}
 		]
 	},
-	// watch: true
+	// watch: true,
+	resolve: {
+		extensions: ['.js', '.vue', '.json', 'scss'],
+		alias: {
+			vue$: "vue/dist/vue.esm.js"
+		}
+	}
 };
