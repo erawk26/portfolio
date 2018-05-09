ccee40b9d7ab27ed23afb93fd8ef10096a8ad572
erik <erik@hark.bz>
2018-05-08 20:11:26 -0400

    missing image on compile UGH

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 Vue.component('portfolio-card', function (resolve) {//this is loading asynchronously so the images don't slow the site down
-	require(['./components/Card.vue'], resolve)
+	require(['./components/Card.vue'], resolve);
 });
 new Vue({
 	render: h => h(App),
