dbf4636b53c7182e147593fd0d26063194c92989
unknown <erawk26@gmail.com>
2019-03-03 17:11:35 -0500

    panel alignment, deleted old files

--- src/js/App.vue
+++ src/js/App.vue
@@ -15,104 +15,108 @@
 </template>
 
 <script>
-	import Projects from './projects.js';
-	//import Card from './components/Card.vue';
+import Projects from "./projects.js";
+//import Card from './components/Card.vue';
 
-	export default {
-		name: 'App',
-		data: () => ({
-			message: 'You loaded this page on ' + new Date().toLocaleString(),
-			active: 'tybee',
-			jobs: Projects.jobs
-		})
-	};
+export default {
+  name: "App",
+  data: () => ({
+    message: "You loaded this page on " + new Date().toLocaleString(),
+    active: "tybee",
+    jobs: Projects.jobs
+  })
+};
 </script>
 
 <style lang="scss" scoped>
-	@import '../scss/init';
+@import "../scss/init";
 
-	.vue-portfolio {
-		min-height: 100vh;
-		padding: 5%;
-		.flex-wrapper {
-			@include set-max-width;
-			@include flex(center, flex-start, $wrap: wrap);
-		}
-		a {
-			display: block;
-			box-sizing: border-box;
-			text-decoration: none;
-			color: black;
-		}
-		.port-head {
-			margin-bottom: 25px;
-			hr {
-				border: none;
-				border-bottom: 1px solid #000;
-				width: 130px;
-				margin: 10px auto;
-			}
-			text-align: center;
-			width: 100%;
-			> * {
-				margin: 0;
-			}
-		}
-	}
+.vue-portfolio {
+  min-height: 100vh;
+  padding: 130px 5% 5%;
+  .flex-wrapper {
+    @include set-max-width;
+    @include flex(center, flex-start, $wrap: wrap);
+  }
+  a {
+    display: block;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: black;
+  }
+  .port-head {
+    margin-bottom: 25px;
+    hr {
+      border: none;
+      border-bottom: 1px solid #000;
+      width: 130px;
+      margin: 10px auto;
+    }
+    text-align: center;
+    width: 100%;
+    > * {
+      margin: 0;
+    }
+  }
+}
 
-	.portfolio-list {
-		@extend %ul-nostyle;
-		margin-bottom: 5px;
-		@include flex($wrap: wrap);
-		@media (min-width: 600px) {
-			//margin-bottom: 15px;
-			width: 175px;
-			margin-right: 25px;
-			@include flex(flex-start, stretch, $direction: column);
-		}
-		> h4 {
-			display: block;
-			width: 100%;
-			//margin-bottom: 5px;
-		}
-		li {
-			@include marding(5px, 0);
-			display: inline-block;
-			.wrapper {
-				cursor: pointer;
-				padding: 5px;
-				position: relative;
-				&.active {
-					background-color: rgba($active, 0.5);
-					color: white;
-					transition: background 0.5s ease;
-				}
-				.hov {
-					display: block;
-					position: absolute;
-					width: 100%;
-					height: 100%;
-					top: 0;
-					left: 0;
-					z-index: -1;
-					content: "";
-				}
+.portfolio-list {
+  @extend %ul-nostyle;
+  margin-bottom: 5px;
+  @include flex($wrap: wrap);
+  @media (min-width: 600px) {
+    //margin-bottom: 15px;
+    width: 175px;
+    margin-right: 25px;
+    @include flex(flex-start, stretch, $direction: column);
+  }
+  > h4 {
+    display: block;
+    width: 100%;
+    //margin-bottom: 5px;
+  }
+  li {
+    @include marding(5px, 0);
+    display: inline-block;
+    .wrapper {
+      cursor: pointer;
+      padding: 5px;
+      position: relative;
+      &.active {
+        background-color: rgba($active, 0.5);
+        color: white;
+        transition: background 0.5s ease;
+      }
+      .hov {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        content: "";
+      }
 
-				&:hover {
-					.title{color: white;}
-					.hov{background-color: rgba($hover, 0.5);}
-				}
-			}
-			img.logo {
-				display: block;
-				position: relative;
-				z-index: 1;
-				height: 75px;
-				width: auto;
-				max-width: 100%;
-				margin: auto;
-				padding: 15px;
-			}
-		}
-	}
+      &:hover {
+        .title {
+          color: white;
+        }
+        .hov {
+          background-color: rgba($hover, 0.5);
+        }
+      }
+    }
+    img.logo {
+      display: block;
+      position: relative;
+      z-index: 1;
+      height: 75px;
+      width: auto;
+      max-width: 100%;
+      margin: auto;
+      padding: 15px;
+    }
+  }
+}
 </style>
